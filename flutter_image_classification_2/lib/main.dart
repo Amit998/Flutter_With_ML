5d7ce1ac2eff11668b15280959e1b1aec7623e8d
amit998 <damitlucky998@gmail.com>
2021-03-19 12:25:21 +0530

    added

---
+++ flutter_image_classification_2/lib/main.dart
@@ -0,0 +1,12 @@
+// import 'dart:io';
+// import 'dart:typed_data';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+
+import 'homePage.dart';
+
+void main() {
+  runApp(MyHomePage());
+}
