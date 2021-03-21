a76ccb98624489e3ff94f9bd933f39daa9c223ba
amit998 <damitlucky998@gmail.com>
2021-03-21 12:18:13 +0530

    added

---
+++ pose_estimator/lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_object_detection/my_home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
